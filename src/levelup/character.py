b17d92d05907b021296ba8eed4fd3a2865c4357a
Todd Hintzmann <thintzmann@me.com>
2023-03-30 23:09:37 +0000

    added a few unit tests to character - get_name and enter_map

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -15,9 +15,19 @@ class Character:
 
     def __init__(self, name: str):
         self.name = name or DEFAULT_CHARACTER_NAME
+        
+    def get_name(self):
+        return self.name
+
+    def get_position(self):
+        return self.position
+#        print(self.position)
 
     def enter_map(self, game_map: GameMap):
-        pass
+        self.map = game_map
+        self.position = self.map.starting_position
+#        print(self.position)
+        return self.position
 
     def move(self, direction: Direction):
         pass
--- tests/test_character.py
+++ tests/test_character.py
@@ -11,3 +11,21 @@ class TestCharacter(TestCase):
         self.assertEqual(testobj.name, expected_name)
         expected_position = None
         self.assertEqual(testobj.position, expected_position)
+
+    def test_get_name_default(self):
+         FakeCharacter = Character(name=None)
+         self.assertEqual(FakeCharacter.get_name(), "Character") 
+
+    def test_get_name_Shorsey(self):
+         FakeCharacter = Character(name="Shorsey")
+         self.assertEqual(FakeCharacter.get_name(), "Shorsey")
+
+    def test_enter_map(self):
+        FakeCharacter = Character(name="Shorsey")
+        FakeMap = GameMap()
+        FakeCharacter.enter_map(FakeMap)
+        pos = FakeCharacter.get_position()
+        print(pos)
+        #Should be born on 0,0
+        self.assertEqual(Position(0,0), pos)
+    
