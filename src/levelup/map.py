13c5301f50937cc2065d85d9b3880d4b374b8d6c
Josh Sawyers <jogrsa@gmail.com>
2023-03-30 20:45:19 +0000

    Add Map init and get_postions tests

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -20,7 +20,22 @@ class GameMap:
         self.create_positions()
 
     def create_positions(self) -> None:
-        pass
+        # Create pos array
+        x_axis = self.size[0]
+        y_axis = self.size[1]
+
+        self.positions = []
+        x_positions = [x for x in range(x_axis)]
+        y_positions = [y for y in range(y_axis)]
+        
+        for x in x_positions:
+            for y in y_positions:
+                self.positions.append(Position(x, y))
+        
+        self.position_count = len(self.positions)
+
+    def get_postions(self):
+        return self.positions
 
     def is_valid_position(self, position: Position) -> bool:
         pass
--- tests/test_map.py
+++ tests/test_map.py
@@ -1,7 +1,19 @@
 from unittest import TestCase
+from typing import List
 from levelup.map import GameMap
+from levelup.position import Position
 
 
 class TestMap(TestCase):
     def test_init(self):
-        pass
+        testmap = GameMap()
+        self.assertEqual(testmap.starting_position, Position(0, 0))
+        self.assertEqual(testmap.position_count, 100)
+        for pos in testmap.positions:
+            self.assertIsInstance(pos, Position)
+
+    def test_get_positions(self):
+        testmap = GameMap()
+        positions = testmap.get_postions()
+        for pos in positions:
+            self.assertIsInstance(pos, Position)
